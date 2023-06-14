6ec08d4ed8568313c24ba03e3cb2d10e83797c1a
Flávia Hespanhol <fghespanhol@hotmail.com>
2023-06-14 12:49:09 -0300

    requisito-5

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -62,6 +62,24 @@ function Form({ data, setForm, handleInputChange,
       </label>
       <button type="submit" disabled={ !isFormValid }>Cadastrar</button>
       <button onClick={ hidindForm }>Cancelar</button>
+      <div>
+        {data.senha.length >= 8
+          ? (<p className="valid-password-check">Possuir 8 ou mais caracteres</p>)
+          : (<p className="invalid-password-check">Possuir 8 ou mais caracteres</p>)}
+
+        {data.senha.length > 16
+          ? (<p className="invalid-password-check">Possuir até 16 caracteres</p>)
+          : (<p className="valid-password-check">Possuir até 16 caracteres</p>)}
+
+        {/(?=.*[a-z])/.test(data.senha) && /(?=.*\d)/.test(data.senha)
+          ? (<p className="valid-password-check">Possuir letras e números</p>)
+          : (<p className="invalid-password-check">Possuir letras e números</p>)}
+
+        {/(?=.*\W+)/.test(data.senha)
+          ? (<p className="valid-password-check">Possuir algum caractere especial</p>)
+          : (<p className="invalid-password-check">Possuir algum caractere especial</p>)}
+
+      </div>
 
     </form>
   );
