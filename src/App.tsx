ee7e9f7018e98bae924c80e166f827aa83fc3b5d
Flávia Hespanhol <fghespanhol@hotmail.com>
2023-06-13 21:37:56 -0300

    requisito-4

--- src/App.tsx
+++ src/App.tsx
@@ -14,12 +14,54 @@ function App() {
     setMostraBotão(true);
   };
 
+  const [data, setData] = useState({
+    nomeServiço: '',
+    login: '',
+    senha: '',
+    url: '',
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setData({
+      ...data,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setData({
+      nomeServiço: '',
+      login: '',
+      senha: '',
+      url: '',
+    });
+  };
+
+  const validateForm = () => {
+    const validarNomeServiço = data.nomeServiço.length > 0;
+    const validarLogin = data.login.length > 0;
+    const regex = /(?=.*[a-z])(?=^.{8,16}$)((?=.*\d)(?=.*\W+))(?![.\n]).*$/;
+    const validarSenha = regex.test(data.senha);
+
+    return validarNomeServiço && validarLogin && validarSenha;
+  };
+
+  const isFormValid = validateForm();
+
   return (
     <>
 
       <h1>Gerenciador de senhas</h1>
       {mostraBotão ? <Button onClick={ handleButton } />
-        : <Form formOculto={ handleForm } />}
+        : <Form
+            data={ data }
+            setForm={ handleForm }
+            handleInputChange={ handleChange }
+            handleSubmitForm={ handleSubmit }
+            isFormValid={ isFormValid }
+        />}
     </>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,31 +1,67 @@
+import React from 'react';
+
 type FormProps = {
-  formOculto: React.Dispatch<React.SetStateAction<boolean>>
+  data: {
+    nomeServiço: string;
+    login: string;
+    senha: string;
+    url: string;
+  };
+
+  setForm: React.Dispatch<React.SetStateAction<boolean>>;
+  handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleSubmitForm: (e: React.FormEvent<HTMLFormElement>) => void;
+  isFormValid: boolean;
 };
 
-function Form({ formOculto }: FormProps) {
-  const ocultandoForm = () => {
-    formOculto(true);
+function Form({ data, setForm, handleInputChange,
+  handleSubmitForm, isFormValid }: FormProps) {
+  const hidindForm = () => {
+    setForm(true);
   };
   return (
-    <form action="">
+    <form action="" onSubmit={ handleSubmitForm }>
       <label htmlFor="Nome serviço">
         Nome do serviço
-        <input id="Nome serviço" type="text" />
+        <input
+          id="Nome serviço"
+          type="text"
+          name="nomeServiço"
+          value={ data.nomeServiço }
+          onChange={ handleInputChange }
+          required
+        />
       </label>
       <label htmlFor="Login">
         Login
-        <input id="Login" type="text" />
+        <input
+          id="Login"
+          type="text"
+          name="login"
+          value={ data.login }
+          onChange={ handleInputChange }
+          required
+        />
       </label>
       <label htmlFor="Senha">
         Senha
-        <input id="Senha" type="password" />
+        <input
+          id="Senha"
+          type="password"
+          name="senha"
+          onChange={ handleInputChange }
+          value={ data.senha }
+        />
       </label>
       <label htmlFor="Url">
         URL
-        <input id="Url" type="text" />
+        <input
+          id="Url"
+          type="text"
+        />
       </label>
-      <button>Cadastrar</button>
-      <button onClick={ ocultandoForm }>Cancelar</button>
+      <button type="submit" disabled={ !isFormValid }>Cadastrar</button>
+      <button onClick={ hidindForm }>Cancelar</button>
 
     </form>
   );
